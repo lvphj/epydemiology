18ca0de436af4421762ed08c834d7554f7e23761
Philip Jones <phjones@me.com>
2017-04-02 13:52:22 +0100

    Restructured for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = ‘epydemiology’,
+    packages = [‘epydemiology’], # this must be the same as the name above
+    version = '0.1’,
+    description = ‘Python code for epidemiology’,
+    author = ‘Phil Jones’,
+    author_email = ‘phjones@me.com’,
+    url = 'https://github.com/lvphj/epydemiology', # use the URL to the github repo
+    download_url = 'https://github.com/lvphj/epydemiology/archive/0.1.tar.gz', # I'll explain this in a second
+    keywords = [‘epidemiology’,’case-control study’,’odds ratio’,’relative risk’,’risk ratio’], # arbitrary keywords
+    classifiers = [],
+)
