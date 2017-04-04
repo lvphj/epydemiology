3e8e263e3dfacc864b30e607ce8fb6dc16d40cb3
Phil Jones <p.h.jones@liverpool.ac.uk>
2017-04-04 02:57:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,37 +10,34 @@ try:
 except (IOError, ImportError):
     long_description = ''
 
-setup(
-    name = 'epydemiology',
-    packages = find_packages(),
-    # packages = ['epydemiology'], # this must be the same as the name above
-    # packages=find_packages(exclude=[list_of_things_to_exclude]),
-    version = '0.1.3',
-    description = 'A library of Python code for epidemiologists',
-    long_description = long_description,
-    author = 'Phil Jones',
-    author_email = 'phjones@me.com',
-    url = 'https://github.com/lvphj/epydemiology', # use the URL to the github repo
-    download_url = 'https://github.com/lvphj/epydemiology/archive/0.1.3.tar.gz',
-    license = 'MIT',
-    keywords = ['epidemiology','case-control study','odds ratio','relative risk','risk ratio'],
-    classifiers = [# How mature is this project? Common values are
-                   #   3 - Alpha
-                   #   4 - Beta
-                   #   5 - Production/Stable
-                   'Development Status :: 3 - Alpha',
-                   
-                   # Indicate who your project is intended for
-                   'Intended Audience :: Science/Research',
-                   'Topic :: Scientific/Engineering :: Medical Science Apps.',
-                   
-                   # Pick your license as you wish (should match "license" above)
-                   'License :: OSI Approved :: MIT License',
-                   
-                   # Specify the Python versions you support here. In particular, ensure
-                   # that you indicate whether you support Python 2, Python 3 or both.
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.4',
-                   ],
-    install_requires=['pandas>=0.19.2'],
-)
+setup(name = 'epydemiology',
+      packages = ['epydemiology'], # this must be the same as the name above
+      # packages=find_packages(exclude=[list_of_things_to_exclude]),
+      version = '0.1.3',
+      description = 'A library of Python code for epidemiologists',
+      long_description = long_description,
+      author = 'Phil Jones',
+      author_email = 'phjones@me.com',
+      url = 'https://github.com/lvphj/epydemiology', # use the URL to the github repo
+      download_url = 'https://github.com/lvphj/epydemiology/archive/0.1.3.tar.gz',
+      license = 'MIT',
+      keywords = ['epidemiology','case-control study','odds ratio','relative risk','risk ratio'],
+      classifiers = [# How mature is this project? Common values are
+                     #   3 - Alpha
+                     #   4 - Beta
+                     #   5 - Production/Stable
+                     'Development Status :: 3 - Alpha',
+                     
+                     # Indicate who your project is intended for
+                     'Intended Audience :: Science/Research',
+                     'Topic :: Scientific/Engineering :: Medical Science Apps.',
+                     
+                     # Pick your license as you wish (should match "license" above)
+                     'License :: OSI Approved :: MIT License',
+                     
+                     # Specify the Python versions you support here. In particular, ensure
+                     # that you indicate whether you support Python 2, Python 3 or both.
+                     'Programming Language :: Python :: 3',
+                     'Programming Language :: Python :: 3.4'],
+      install_requires=['pandas>=0.19.2'],
+      )
