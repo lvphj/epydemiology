ed16df9e6306993a56731baa3385a7d904095ab4
Phil Jones <p.h.jones@liverpool.ac.uk>
2017-05-29 16:49:38 +0100

    Includes phjReadTextFromFile() function

--- epydemiology/__init__.py
+++ epydemiology/__init__.py
@@ -3,4 +3,5 @@ from .phjRROR import phjRelativeRisk
 from .phjSelectData import phjSelectCaseControlDataset
 from .phjGetData import phjReadDataFromExcelNamedCellRange
 from .phjGetDBData import phjGetDataFromDatabase
+from .phjGetDBData import phjReadTextFromFile
 from .phjMatrices import phjBinaryVarsToSquareMatrix
