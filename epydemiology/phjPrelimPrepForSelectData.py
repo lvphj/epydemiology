b8e12e0aad7b6fe64255e435c047e3b8a3b6b6e0
Phil Jones <p.h.jones@liverpool.ac.uk>
2017-11-16 17:13:51 +0000

    Update phjPrelimPrepForSelectData.py

--- epydemiology/phjPrelimPrepForSelectData.py
+++ epydemiology/phjPrelimPrepForSelectData.py
@@ -15,6 +15,18 @@ import pandas as pd
 #     | consultID | patientID | case | var1 | var2 | var3 |
 #     |-----------|-----------|------|------|------|------|
 #     |      1001 |      7324 |   no |    a |  454 |  low |
-#     |      1002 |      7324 |  yes |    a |  454 |  low |
-#     |      1003 |      7324 |   no |    a |  454 |  low |
-#     |      1004 |      7324 |   no |    a |  454 |  low |
+#     |      1002 |      7324 |  yes |    b |  345 |  low |
+#     |      1003 |      7324 |   no |    c |  879 |  low |
+#     |      1004 |      9767 |  yes |    a |  276 |  mid |
+#     |      1005 |      9767 |  yes |    b |  478 |  mid |
+#     |      1006 |      3452 |   no |    c |  222 |  mid |
+#     |      1007 |      5322 |   no |    a |  590 |   hi |
+#     |      1008 |      5322 |  yes |    b |  235 |   hi |
+#     |      1009 |      5322 |  yes |    c |  657 |   hi |
+#     etc.
+#
+# Selecting consultation controls
+# -------------------------------
+# The workflow that involves selecting 
+# 
+# 
