2951907b550fd820f09d975b2680921afc4fa850
Philip Jones <phjones@me.com>
2023-04-18 23:00:23 +0100

    Adds alternative DL dist algorithm if pyxdl not available

--- epydemiology/phjCleanUKPostcodes.py
+++ epydemiology/phjCleanUKPostcodes.py
@@ -61,8 +61,10 @@ else:
 # above method because attribute .DistributionNotFound is not present.
 try:
     import pyxdameraulevenshtein as pyxdl
+    pyxdlPresent = True
 except ImportError:
     print("Error: pyxdameraulevenshtein package not installed. Some features may not be available.")
+    pyxdlPresent = False
 
 
 import re
@@ -1096,19 +1098,31 @@ def phjCalcMinDamLevDistAndEdits(x,
     # Convert the numpy array of postcodes to a Pandas dataframe
     phjPostcodeDF = pd.DataFrame(phjRealPostcodeArr,columns = ['pcdMin'])
     
-    # Calculate distance from string to each postcode in dataframe.
-    # In versions of pyxdameraulevenshtein library before 1.7.0, the function used was
-    # damerau_levenshtein_distance_ndarray(). This used np.array.
-    # In version 1.7.0, the function was changed to damerau_levenshtein_distance_seqs()
-    # which uses built-in Python lists.
-    # The following addressess issue #44 on epydemiology GitHub Issues page.
-    if pkg_resources.get_distribution("pyxdameraulevenshtein").version < '1.7.0':
-        phjPostcodeDF['tempDL'] = pyxdl.damerau_levenshtein_distance_ndarray(x[phjNewPostcodeVarName], phjRealPostcodeArr)
+    # Calculate Damerau-Levenshtein distance from entered postcode to all real postcodes
+    # Calculate distance from string to each element in an array.
+    # Ideally use pyxdameraulevenshtein library because faster but if not installed
+    # then use pure Python implementation by Michael Homer available at:
+    # https://web.archive.org/web/20150909134357/http://mwh.geek.nz:80/2009/04/26/python-damerau-levenshtein-distance/
     
+    # Set pyxdlPresent to False for testing purposes, to force the use of an
+    # algorithm different from pyxdameraulevenshtein  - comment out when testing complete
+    #pyxdlPresent = False
+    
+    if pyxdlPresent == True:
+        # In versions of pyxdameraulevenshtein library before 1.7.0, the function used was
+        # damerau_levenshtein_distance_ndarray(). This used np.array.
+        # In version 1.7.0, the function was changed to damerau_levenshtein_distance_seqs()
+        # which uses built-in Python lists.
+        # The following addressess issue #44 on epydemiology GitHub Issues page.
+        if pkg_resources.get_distribution("pyxdameraulevenshtein").version < '1.7.0':
+            phjPostcodeDF['tempDL'] = pyxdl.damerau_levenshtein_distance_ndarray(x[phjNewPostcodeVarName], phjRealPostcodeArr)
+            
+        else:
+            phjPostcodeDF['tempDL'] = pyxdl.damerau_levenshtein_distance_seqs(x[phjNewPostcodeVarName], phjRealPostcodeArr)
+            
     else:
-        phjPostcodeDF['tempDL'] = pyxdl.damerau_levenshtein_distance_seqs(x[phjNewPostcodeVarName], phjRealPostcodeArr)
-        
-        
+        phjPostcodeDF['tempDL'] = mhDamerauLevenshtein(x[phjNewPostcodeVarName], phjRealPostcodeArr)
+            
     # Calculate minimum DL distance
     phjMinDamLevDist = phjPostcodeDF['tempDL'].min(axis = 0)
     
@@ -1140,7 +1154,7 @@ def phjCalcMinDamLevDistAndEdits(x,
                                                                                                                     phjColHeading = 'pcdMin',
                                                                                                                     phjCleanInputStrings = False,
                                                                                                                     phjIncludeEquivalenceEdits = False),axis=1)
-
+        
         
         
                 
@@ -1156,6 +1170,86 @@ def phjCalcMinDamLevDistAndEdits(x,
     print('   Returned list of edits: {0}\n'.format([phjMinDamLevDist,phjPossPostcodesList]))
     
     return pd.Series([phjMinDamLevDist,phjPossPostcodesList],index=['minDamLevDist','bestAlternatives'])
+
+
+def mhDamerauLevenshtein(seq,seqArr):
+    # This function compares a fixed sequence with every element in an array and
+    # produces a list of DL distances using the pure Python implementation of the
+    # Damerau-Levenshtein algorithm by Michael Homer as an alternative to using the
+    # faster pyxdameraulevenshtein library which may not be installable on some systems.
+    phjList = []
+    for iseq in seqArr:
+        phjList = phjList + [dameraulevenshtein(seq,iseq)]
+    return phjList
+
+
+def dameraulevenshtein(seq1, seq2):
+# Pure Python implementation to calculate Damerau-Levenshtein by Michael Homer available at:
+# https://web.archive.org/web/20150909134357/http://mwh.geek.nz:80/2009/04/26/python-damerau-levenshtein-distance/
+# Original implementation in Python 2; could be updated to Python 3 using 2to3 but
+# in the following, the code was updated by hand.
+    """Calculate the Damerau-Levenshtein distance between sequences.
+
+    This distance is the number of additions, deletions, substitutions,
+    and transpositions needed to transform the first sequence into the
+    second. Although generally used with strings, any sequences of
+    comparable objects will work.
+
+    Transpositions are exchanges of *consecutive* characters; all other
+    operations are self-explanatory.
+
+    This implementation is O(N*M) time and O(M) space, for N and M the
+    lengths of the two sequences.
+
+    >>> dameraulevenshtein('ba', 'abc')
+    2
+    >>> dameraulevenshtein('fee', 'deed')
+    2
+
+    It works with arbitrary sequences too:
+    >>> dameraulevenshtein('abcd', ['b', 'a', 'c', 'd', 'e'])
+    2
+    """
+    # codesnippet:D0DE4716-B6E6-4161-9219-2903BF8F547F
+    # Conceptually, this is based on a len(seq1) + 1 * len(seq2) + 1 matrix.
+    # However, only the current and two previous rows are needed at once,
+    # so we only store those.
+#    oneago = None
+#    thisrow = range(1, len(seq2) + 1) + [0]
+#    for x in xrange(len(seq1)):
+#        # Python lists wrap around for negative indices, so put the
+#        # leftmost column at the *end* of the list. This matches with
+#        # the zero-indexed strings and saves extra calculation.
+#        twoago, oneago, thisrow = oneago, thisrow, [0] * len(seq2) + [x + 1]
+#        for y in xrange(len(seq2)):
+#            delcost = oneago[y] + 1
+#            addcost = thisrow[y - 1] + 1
+#            subcost = oneago[y - 1] + (seq1[x] != seq2[y])
+#            thisrow[y] = min(delcost, addcost, subcost)
+#            # This block deals with transpositions
+#            if (x > 0 and y > 0 and seq1[x] == seq2[y - 1]
+#                and seq1[x-1] == seq2[y] and seq1[x] != seq2[y]):
+#                thisrow[y] = min(thisrow[y], twoago[y - 2] + 1)
+#    return thisrow[len(seq2) - 1]
+
+    oneago = None
+    thisrow = list(range(1, len(seq2) + 1)) + [0]
+    for x in range(len(seq1)):
+        # Python lists wrap around for negative indices, so put the
+        # leftmost column at the *end* of the list. This matches with
+        # the zero-indexed strings and saves extra calculation.
+        twoago, oneago, thisrow = oneago, thisrow, [0] * len(seq2) + [x + 1]
+        for y in range(len(seq2)):
+            delcost = oneago[y] + 1
+            addcost = thisrow[y - 1] + 1
+            subcost = oneago[y - 1] + (seq1[x] != seq2[y])
+            thisrow[y] = min(delcost, addcost, subcost)
+            # This block deals with transpositions
+            if (x > 0 and y > 0 and seq1[x] == seq2[y - 1]
+                and seq1[x-1] == seq2[y] and seq1[x] != seq2[y]):
+                thisrow[y] = min(thisrow[y], twoago[y - 2] + 1)
+    return thisrow[len(seq2) - 1]
+
 
 
 def phjCallDLEditsFunction(row,
